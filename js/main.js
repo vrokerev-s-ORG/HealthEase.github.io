21edeec73613543ee903dc2ce92a5dc5ebbcdee2
vrokerev <103162443+vrokerev@users.noreply.github.com>
2023-11-21 23:42:46 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-function scrolll(){
-    const container = document.querySelector(".scroll-test");
-    container.scrollBy(-100, 0);
-}
-function scrollr(){
-    const container = document.querySelector(".scroll-test");
-    container.scrollBy(100, 0);
-}   
