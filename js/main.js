28b352520b4554ebc838c6df349e27fcd5f0b0c1
vrokerev <103162443+vrokerev@users.noreply.github.com>
2023-11-21 23:43:08 -0500

    Add js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function scrolll(){
+    const container = document.querySelector(".scroll-test");
+    container.scrollBy(-100, 0);
+}
+function scrollr(){
+    const container = document.querySelector(".scroll-test");
+    container.scrollBy(100, 0);
+}   
