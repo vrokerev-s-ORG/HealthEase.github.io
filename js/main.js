f26baacac045170612bd5d925d6b4e9a686edbb6
HamesEl <129306594+HamesEl@users.noreply.github.com>
2023-11-24 14:55:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+const benefits = document.querySelector("#benefits")
+const testimonios = document.querySelector("#testimonios")
+const seguimiento = document.querySelector("#seguimiento")
+const suscribete = document.querySelector("#suscribete")
+
+/* Beneficios */
+benefits.addEventListener("click", (b) => {
+    b.preventDefault();
+    const sectionB = document.querySelector(".benefits-section");
+    sectionB.scrollIntoView({behavior:"smooth"})
+})
+
+/* Testimonios */
+testimonios.addEventListener("click", (t) => {
+    t.preventDefault();
+    const sectionT = document.querySelector(".comentarios");
+    sectionT.scrollIntoView({behavior:"smooth"})
+})
+
+/* Seguimiento */
+seguimiento.addEventListener("click", (s) => {
+    s.preventDefault();
+    const sectionS = document.querySelector(".SeguimientoGlobal");
+    sectionS.scrollIntoView({behavior:"smooth"})
+})
+
+/* suscribete */
+suscribete.addEventListener("click", (u) => {
+    u.preventDefault();
+    const sectionU = document.querySelector(".subscribe");
+    sectionU.scrollIntoView({behavior:"smooth"})
+})
+
 
 function scrolll(){
     const container = document.querySelector(".scroll-test");
